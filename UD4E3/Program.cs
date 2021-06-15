738cc2abd36b90fd3df33c11658b23783952a5af
Martina <martinaescorial9@gmail.com>
2021-06-15 21:14:01 +0200

    Agregar archivos de proyecto.

---
+++ UD4E3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD4E3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre = "Martina";
+            String años = "20";
+            Console.WriteLine("Me llamo " + nombre + " y tengo " + años + " años");
+            Console.ReadLine();
+        }
+    }
+}
